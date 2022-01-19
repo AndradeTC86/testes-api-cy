dffd82b9599997e742b82dcdb42716d5abda685e
Thiago Cesar <andrade-tc@uol.com.br>
2022-01-19 19:16:55 -0300

    exercicio automacao

--- cypress/integration/exercicio-api.spec.js
+++ cypress/integration/exercicio-api.spec.js
@@ -1,30 +1,47 @@
 /// <reference types="cypress" />
+import contrato from '../contracts/usuarios.contract'
+var faker = require('faker')
 
-describe('Testes da Funcionalidade Usuários', () => {
-
-    it('Deve validar contrato de usuários', () => {
-         //TODO: 
+describe("Testes da Funcionalidade Usuários", () => {
+  it("Deve validar contrato de usuários", () => {
+    cy.request("usuarios").then((response) => {
+      return contrato.validateAsync(response.body);
     });
+  });
 
-    it('Deve listar usuários cadastrados', () => {
-         //TODO: 
+  it("Deve listar usuários cadastrados", () => {
+    cy.request({
+      method: "GET",
+      url: "usuarios",
+    }).then((response) => {
+      expect(response.status).to.equal(200);
+      expect(response.body).to.have.property("usuarios");
     });
+  });
 
-    it('Deve cadastrar um usuário com sucesso', () => {
-         //TODO: 
-    });
+  it.only("Deve cadastrar um usuário com sucesso", () => {
+    let nomeFaker = faker.name.firstName();
+    let emailFaker = faker.internet.email(nomeFaker);
 
-    it('Deve validar um usuário com email inválido', () => {
-         //TODO: 
-    });
+    cy.cadastrarUsuario(nomeFaker, emailFaker, "senha@123", "false").then(
+      (response) => {
+        expect(response.status).to.equal(201);
+        expect(response.body.message).to.equal(
+          "Cadastro realizado com sucesso"
+        );
+      }
+    );
+  });
 
-    it('Deve editar um usuário previamente cadastrado', () => {
-         //TODO: 
-    });
+  it("Deve validar um usuário com email inválido", () => {
+    //TODO:
+  });
 
-    it('Deve deletar um usuário previamente cadastrado', () => {
-        //TODO: 
-    });
+  it("Deve editar um usuário previamente cadastrado", () => {
+    //TODO:
+  });
 
-
+  it("Deve deletar um usuário previamente cadastrado", () => {
+    //TODO:
+  });
 });
