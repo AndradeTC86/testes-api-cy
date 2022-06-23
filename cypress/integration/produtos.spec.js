7e5941ff544b92d186d9fd243573ec3a9c83884b
Thiago Cesar <andrade-tc@uol.com.br>
2022-06-23 18:03:39 -0300

    Refactoring

--- cypress/integration/produtos.spec.js
+++ cypress/integration/produtos.spec.js
@@ -17,8 +17,7 @@ describe('Testes da Funcionalidade Produtos', () => {
         cy.request({
             method: 'GET',
             url: 'produtos'
-        }).then((response) => {
-            //expect(response.body.produtos[9].nome).to.equal('Produto EBAC 436746')
+        }).then((response) => {            
             expect(response.status).to.equal(200)
             expect(response.body).to.have.property('produtos')
             expect(response.duration).to.be.lessThan(20)
